9000831895fb5c969abf8a343bec9ae951bb3f2d
Carolina Romanos <carolina.romanos.roca@gmail.com>
2021-12-04 17:20:42 +0100

    ref

--- src/pages/NewBeer.jsx
+++ src/pages/NewBeer.jsx
@@ -29,25 +29,25 @@ export default function NewBeer() {
         <Header />
         <form onSubmit={handleSubmit}>
                 <label htmlFor="name">Name:</label><br />
-                <input type="text" id="name" name="name" onChange={handleInput} /><br />
+                <input type="text" name="name" onChange={handleInput} /><br />
                 
                 <label htmlFor="tagline">Tagline: </label><br />
-                <input type="text" id="tagline" name="tagline" onChange={handleInput} /><br />
+                <input type="text" name="tagline" onChange={handleInput} /><br />
                 
                 <label htmlFor="description">Description: </label><br />
-                <input type="text" id="description" name="description" onChange={handleInput} /><br />
+                <input type="text"  name="description" onChange={handleInput} /><br />
                 
                 <label htmlFor="first_brewed">First Brewed:</label><br />
-                <input type="text" id="first_brewed" name="first_brewed" onChange={handleInput} /><br />
+                <input type="text"  name="first_brewed" onChange={handleInput} /><br />
                 
                 <label htmlFor="brewers_tips">Brewers Tips:</label><br />
-                <input type="text" id="brewers_tips" name="brewers_tips" onChange={handleInput} /><br />
+                <input type="text"  name="brewers_tips" onChange={handleInput} /><br />
                 
                 <label htmlFor="attenuation_level ">Attenuation Level:</label><br />
-                <input type="number" id="attenuation_level" name="attenuation_level" onChange={handleInput} /><br />
+                <input type="number"  name="attenuation_level" onChange={handleInput} /><br />
                 
                 <label htmlFor="contributed_by ">Contributed By:</label><br />
-                <input type="text" id="contributed_by" name="contributed_by" onChange={handleInput} /> <br />
+                <input type="text"  name="contributed_by" onChange={handleInput} /> <br />
                 
                 <button type="submit">Submit</button>
             </form>
